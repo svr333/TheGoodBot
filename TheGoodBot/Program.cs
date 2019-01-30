c2df5635d84caf0ef71a714ebd3fee10afb53f33
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-01-30 16:47:05 +0100

    Add project files.

---
+++ TheGoodBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheGoodBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
